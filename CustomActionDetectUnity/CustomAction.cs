b2ce39bb6ab56e5e124abd91831dcdffdbcb0790
ElijahZAwesome <elijahbansley@monroe.k12.wi.us>
2019-06-08 13:12:50 -0500

    Gonna try redoing this shit

--- CustomActionDetectUnity/CustomAction.cs
+++ CustomActionDetectUnity/CustomAction.cs
@@ -17,6 +17,8 @@ namespace DetectUnity
     [RunInstaller(true)]
     public partial class DetectUnity : System.Configuration.Install.Installer
     {
+        private Label label1;
+
         public DetectUnity()
         {
             // STUB
@@ -44,6 +46,21 @@ namespace DetectUnity
         public override void Uninstall(IDictionary savedState)
         {
             base.Uninstall(savedState);
+        }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+
         }
         // EditorApplication.applicationPath
     }
